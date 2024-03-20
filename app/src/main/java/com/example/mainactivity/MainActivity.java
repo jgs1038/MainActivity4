ba0a6daa8e26aa5861474c641ecb087061caef41
JGS <jgs1038@alu.ubu.es>
2024-03-20 16:11:23 +0100

    cositas

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -3,18 +3,36 @@ package com.example.mainactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
-
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
+    private EditText et2;
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        et1 = (EditText) findViewById(R.id.txt_num1);
+        et2 = (EditText) findViewById(R.id.txt_num2);
+        tv1 = (TextView) findViewById(R.id.txt_resultado);
     }
 
+    public void Sumar(View view) {
+        String valor1 = et1.getText().toString();
+        String valor2 = et2.getText().toString();
+        int num1 = Integer.parseInt(valor1);
+        int num2 = Integer.parseInt(valor2);
+        int sum = num1 + num2;
+        String result = String.valueOf(sum);
+        tv1.setText(result);
+    }
     protected void onStart(){
         super.onStart();
         Toast.makeText(this,"onStart",Toast.LENGTH_SHORT).show();
